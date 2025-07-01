eb57b74335f5f552caa647d0e1dcb5e3acc25fc5
Benzema242 <rubensscutt91@live.com>
2025-07-01 00:54:46 -0400

    added word

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,6 +5,6 @@ class Program
     static void Main(string[] args)
     {
         // This is a simple C# program that prints "Hello World!" to the console.
-        Console.WriteLine("Hello World!!!!");
+        Console.WriteLine("Hello Sandbox World!!!!");
     }
 }
