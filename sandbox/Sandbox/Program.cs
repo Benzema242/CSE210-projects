94ba3a13af0bd66c99765b401212a3c27bd87c03
Benzema242 <rubensscutt91@live.com>
2025-07-01 00:44:19 -0400

    chaned the display message

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Sandbox Project.");
+        // This is a simple C# program that prints "Hello World!" to the console.
+        Console.WriteLine("Hello World!");
     }
 }
